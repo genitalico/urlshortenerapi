22b9a778247cd9be527c1bf7a956f99c3d3c3b2e
Galileo Teco <g@80bits.com>
2018-06-16 16:56:01 -0500

    I added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,12 +14,13 @@ var app = express();
 var connection = require('./mongodb/connection');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 if (process.env.NODE_ENV != "production" || process.env.NODE_ENV != "staging") {
   app.use(logger('dev'));
 }
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
